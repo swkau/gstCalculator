5892036e2d64319f9623e642821b04232d780337
Kau Si Wei <kausiwei@gmail.com>
2016-07-09 16:01:08 +0800

    updated input amount and gst amount and total amount

--- gstCalculator/ViewController.swift
+++ gstCalculator/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var answer2Label: UILabel!
+    @IBOutlet weak var answer3Label: UILabel!
     @IBAction func calculateButton(sender: UIButton) {
     
         if(textField != nil){
-    let calculateAmount = Double(textField.text!)!*1.07
-        answerLabel.text = String(calculateAmount)
+    let calculateAmount = Double(textField.text!)!
+    let calculate2Amount = Double(textField.text!)!*0.07
+    let calculate3Amount = Double(textField.text!)!*1.07
+        answerLabel.text = "Input amount: $" + String(calculateAmount)
+        answer2Label.text = "GST amount: $" + String(calculate2Amount)
+        answer3Label.text = "Total after GST: $" + String(calculate3Amount)
         textField.text = ""
         }
         
